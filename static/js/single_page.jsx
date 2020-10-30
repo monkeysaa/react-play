8d937d9e1f96024d591caffb9224c441e8bacf0a
Alison Cordeiro <acordeiro@gmail.com>
2020-10-29 23:07:06 -0700

    Basic page - links with React

--- static/js/single_page.jsx
+++ static/js/single_page.jsx
@@ -1,23 +1,21 @@
 "use strict";
 
-function ClearHTML(e) {
-  e.preventDefault();
-  document.getElementById("home").innerHTML = "";
-  document.getElementById("boo").style.display = "block";
-}
-
 function Homepage() {
   return (
     <React.Fragment>
-      <section id="home">
-        <p>Click <a href='/profile' onClick={ClearHTML}>here </a> to see your profile page!</p>
+      <section id="welcome">
+        <p>Click for <a href='/profile' onClick={DisplayProfile}> your profile page! </a> </p>
+        <p>Link to <a href='/view_users' onClick={DisplayUsers}> display users. </a> </p>
         <p><img src='/static/high5.jpg' /></p>
         <button type="button" onClick={ClearHTML} > Clear </button>
-      </section>
+      </section>      
       <div id="boo" style={{ display: 'none' }}>
         Boo!
         <p><img src='/static/boo.gif' /></p>
       </div>
+      <section id="profile" style={{ display: 'none' }}>
+        <p>(Your future profile)</p>
+      </section>
     </React.Fragment>
   );
 }
@@ -46,22 +44,42 @@ function PrintUsers() {
 
   for (let [key, value] of Object.entries(users)) {
 //    console.log(key, value);
-    content.push(value);
+    content.push(<p>{value}</p>);
   }
 
   return <div>{content}</div>
 }
 
 
+function ClearHTML() {
+  document.getElementById("welcome").innerHTML = "";
+  document.getElementById("users").innerHTML = "";
+  document.getElementById("boo").style.display = "block";
+}
+
+function DisplayProfile(e) {
+  e.preventDefault();
+  document.getElementById("welcome").innerHTML = "";
+  document.getElementById("boo").style.display = "block";
+  document.getElementById("profile").style.display = "block";
+}
+
+function DisplayUsers(e) {
+  e.preventDefault();
+  document.getElementById("welcome").innerHTML = "";
+  document.getElementById("users").style.display = "block";
+  ReactDOM.render(
+    <PrintUsers />,
+    document.getElementById('users')
+  );
+}
+
 ReactDOM.render(
   <Homepage />,
   document.querySelector('#main')
 );
 
-ReactDOM.render(
-  <PrintUsers />,
-  document.querySelector('#users')
-)
+
 // render Homepage and then hierarchy
 // Within Homepage, render Ghost + render Users
 // Show ghost --> Show Users --> Show Users in React --> Add User --> Modals? --> CSS
